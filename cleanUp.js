22a0f8abf832d402e8283bbc88eb2bdd9d709a1f
Cade Chaney <cadechaney@live.com>
2023-01-12 12:34:06 -0600

    Add answer to questions

--- cleanUp.js
+++ cleanUp.js
@@ -9,38 +9,40 @@
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
-
+nameQuestion();
+// I added a semi-colon to the end of the code block and the end of the call.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 console.log(sum);
-      }
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// I had to fix the position of the closing curly bracket
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto(){
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
-
+// Had to change position of last curly bracket to be at the bottom.
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+  console.log(`the average is: ${avg}`)
+}
+  // Had to fix first curly bracket to be next on line 41 next to the function. I indented the var sums correctly. 
+  // Finally I had to fix position of last curly bracket to be in line with the function.
