2f316d795999306c7ffdcf29b97b9e5129cacb30
Cade Chaney <cadechaney@live.com>
2023-01-12 13:25:03 -0600

    Add questions 2 and 3

--- functions.js
+++ functions.js
@@ -6,12 +6,27 @@ function printGreeting(){
   printGreeting();
   printGreeting();
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function greetMe(first, last) {
+    console.log(`Good Morning ${first}`);
+  }
+  greetMe("Cade", "Chaney");
 
+// I believe I did this question correctly
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+var job = "UPS"
+function add(num1, num2) {
+  var sum = num1 - num2;
+  return sum;
+}
+
+console.log(`The max pay for ${job} is $120,000 and minis  $40,000. The range is ${add(120000, 40000)}.`);
 
 // 4: Write a function that satifies the following interaction pattern:
+
+
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
