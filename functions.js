dde1960b71b047e76f02f2f4f0aaf59038b08017
Cade Chaney <cadechaney@live.com>
2023-01-13 05:26:15 -0600

    Add feedback from kaitlyn

--- functions.js
+++ functions.js
@@ -1,43 +1,44 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-function printGreeting(){
+function printGreeting() {
     console.log("Hello friend!"); 
 }
-  printGreeting();
-  printGreeting();
-  printGreeting();
+printGreeting();
+printGreeting();
+printGreeting();
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 function greetMe(first, last) {
     console.log(`Good Morning ${first}`);
-  }
-  greetMe("Cade", "Chaney");
+}
+greetMe("Cade");
 
 // I believe I did this question correctly
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
-var job = "UPS"
-function add(num1, num2) {
-  var sum = num1 - num2;
-  return sum;
-}
+function jobPay(first, second) {
+  console.log(`Average pay at ${first} is $${second}`);
 
-console.log(`The max pay for ${job} is $120,000 and minis  $40,000. The range is ${add(120000, 40000)}.`);
+}
+jobPay("UPS", 1500 - 500);
+
+
 
 // 4: Write a function that satifies the following interaction pattern:
 
-function checkStock(itemQuantity, item){
-    if (itemQuantity === 0) {
+function checkStock(itemQuantity, item) {
+  if (itemQuantity === 0) {
     console.log(`${item} -OUT of stock`);
   } else if (itemQuantity < 4) {
     console.log (`${item} - is running LOW`);
   } else {
-      console.log(`${item} is stocked`);
-  }}
+    console.log(`${item} is stocked`);
+  }
+}
   
-  checkStock(4, "Coffee");
-  checkStock(3, "Tortillas");
-  checkStock(0, "Cheese");
-  checkStock(1, "Salsa");
+checkStock(4, "Coffee");
+checkStock(3, "Tortillas");
+checkStock(0, "Cheese");
+checkStock(1, "Salsa");
 // => "Coffee is stocked"
 
 checkStock(3, "Tortillas");
